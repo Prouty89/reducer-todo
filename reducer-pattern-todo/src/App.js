2ef1b5edf6ce5d791ceb45de65f0b75fbaa77f39
Blake <bpro0821@gmail.com>
2019-08-13 15:41:27 -0500

    User can add/delete todos

--- reducer-pattern-todo/src/App.js
+++ reducer-pattern-todo/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import Todo from './components/Todo'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      Snap Peas r us
+     <Todo />
     </div>
   );
 }
--- reducer-pattern-todo/src/components/Todo.js
+++ reducer-pattern-todo/src/components/Todo.js
@@ -1,26 +1,51 @@
-import { useState, useReducer } from 'react';
+import React, { useState, useReducer } from 'react';
 
 import { initialState, todoReducer } from '../reducers/todoReducer'
 
 const Todo = () => {
-const [newTodo, setNewTodo] = useState();
-
-const [state, dispatch] = useReducer(todoReducer, initialState);
-}
-
-const handleChanges = e => {
-    setNewTodo(e.target.value)
-};
-
-
-return(
-    <div>
-        {!state.editing ? (
-            <h1>
-                {state.todo}
-            </h1>
-        )}
-    </div>
-
-)
-
+    const [todo, newTodo] = useState('');
+    const [state, dispatch] = useReducer(todoReducer, initialState);
+  
+     const handleChange = event => {
+      newTodo(event.target.value);
+    };
+  
+     const handleSubmit = event => {
+      event.preventDefault();
+      if (todo !== ''){
+      dispatch({ type: 'NEW_TODO', payload: todo });
+      newTodo('');
+      }
+    };
+  
+     return (
+      <div className='todo-app'>
+        <h1>Reduce(you)r Todos!</h1>
+  
+  
+         {state.todos.map(todo => (
+          <div
+            key={todo.id}
+            onClick={() => dispatch({ type: 'TOGGLE_TODO', payload: todo })}>
+            <p>Todo: {todo.item}</p>
+            <p>Completed: {todo.completed ? 'TRUE' : 'FALSE'}</p>
+          </div>
+        ))}
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            name='todo'
+            value={todo}
+            onChange={handleChange}
+            placeholder='Add a todo!'
+          />
+          <button type='submit'>Submit</button>
+        </form>
+         <button onClick={() => dispatch({ type: 'REMOVE_TODO' })}>
+          Delete Todo
+        </button>
+      </div>
+    );
+  };
+  
+   export default Todo;
